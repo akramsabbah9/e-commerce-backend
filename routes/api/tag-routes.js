991fe32d07c959d84f30172e9209e4da9e3838d9
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-07 11:41:54 -0800

    added POST and DELETE routes to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,7 +32,7 @@ router.get("/:id", (req, res) => {
     .then(dbTagData => {
         if (!dbTagData)
             return res.status(404).json({ message: "No tag found with this id" });
-        res.json(dbTagData)
+        res.json(dbTagData);
     })
     .catch(err => {
         console.log(err);
@@ -40,16 +40,39 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// create a new tag
 router.post("/", (req, res) => {
-    // create a new tag
+    // expects {tag_name} in req.body
+    Tag.create({
+        tag_name: req.body.tag_name
+    })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
     // update a tag"s name by its `id` value
 });
 
+// delete a tag by its id
 router.delete("/:id", (req, res) => {
-    // delete on tag by its `id` value
+    Tag.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbTagData => {
+        if (!dbTagData)
+            return res.status(404).json({ message: "No tag found with this id" });
+        res.json(dbTagData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
